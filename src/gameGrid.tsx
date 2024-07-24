9da34d8fd2504f7008696bda5d88f4f804d528a5
conmart <connor.a.martinelli@gmail.com>
2024-07-23 17:47:44 -0700

    basic game grid

---
+++ src/gameGrid.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import GridRow from './gridRow';
+
+const GameGrid = () => {
+  return (
+    <div className="gameGrid">
+      <GridRow />
+      <GridRow />
+      <GridRow />
+      <GridRow />
+      <GridRow />
+      <GridRow />
+      <GridRow />
+    </div>
+  );
+};
+
+export default GameGrid;
---
+++ src/gridSquare.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GridSquare = () => {
+  return <div className="gridSquare"></div>;
+};
+
+export default GridSquare;
