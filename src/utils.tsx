83fed370917e95d3a5d82458e7a12f7f3f493d22
conmart <connor.a.martinelli@gmail.com>
2024-07-27 14:31:40 -0700

    block break logic

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,19 +1,49 @@
-let explosionCoordinates:number[][] = [];
+let explosionCoordinates = new Set<number[]>();
+let gameGrid: number[][] = [];
 
 const checkExplosionsColumn = (column: number[], columnIndex: number) => {
   const columnHeight = nextZero(column) === -1 ? 7 : nextZero(column);
   if (columnHeight > 0) {
     column.forEach((value, index) => {
       if (value === columnHeight) {
-        explosionCoordinates.push([columnIndex, index]);
+        explosionCoordinates.add([columnIndex, index]);
       }
     });
   }
 };
 
-export const processExplosions = (gameGrid: number[][]) => {
-  gameGrid.forEach((column, index) => checkExplosionsColumn(column, index));
-  console.log(explosionCoordinates)
+const explodeBlock = (column: number, row: number) => {
+  console.log(column, row, 'yo');
+  if (column > -1 && column < 7 && row > -1 && row < 7) {
+    const currentValue = gameGrid[column][row];
+    if (currentValue === 8) {
+      gameGrid[column][row] = randomBallValue();
+    } else if (currentValue===9) {
+      gameGrid[column][row] = 8
+    }
+  }
+};
+
+const explodeSurroundingBlocks = () => {
+  explosionCoordinates.forEach((coordinate) => {
+    const column = coordinate[0];
+    const row = coordinate[1];
+    explodeBlock(column - 1, row);
+    explodeBlock(column + 1, row);
+    explodeBlock(column, row - 1);
+    explodeBlock(column, row + 1);
+  });
+  explosionCoordinates.clear();
+};
+
+export const processExplosions = (gameGridFromState: number[][]) => {
+  gameGrid = gameGridFromState;
+  gameGridFromState.forEach((column, index) =>
+    checkExplosionsColumn(column, index)
+  );
+  console.log(explosionCoordinates);
+  console.log(gameGrid);
+  explodeSurroundingBlocks();
   return gameGrid;
 };
 
